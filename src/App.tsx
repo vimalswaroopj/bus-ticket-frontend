795e079b6f8303092a268517d741a7ee27e88d9a
vimalswaroopj <42696640+vimalswaroopj@users.noreply.github.com>
2024-02-03 21:28:05 +0530

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from "react";
 import { Dashboard } from "./ui/Dashboard/Dashboard";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NavigationBar } from "./ui/NavigationBar/NavigationBar";
 import { Seatbooking } from "./ui/SeatBooking/SeatBooking";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Seatbooking />,
-  },
-  {
-    path: "dashboard",
-    element: <Dashboard />,
-  },
-]);
 function App() {
   return (
     <>
       <NavigationBar />
-      <React.StrictMode>
-        <RouterProvider router={router} />
-      </React.StrictMode>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Seatbooking />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
